ad120c1364d7b33ab638b3508a7bb6d417e86146
Dmitry Kralichkin <dm_krl@mail.ru>
2024-04-11 01:59:13 +1200

    init app

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import './index.css';
+
+function App() {
+    return (
+        <div className="app">
+            <div className="wrapper">
+                <div className="container">Future app delivery</div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
