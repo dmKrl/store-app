66016977899b8c26fea4aa34c1952b4e25174c3c
Dmitry Kralichkin <dm_krl@mail.ru>
2024-08-06 15:26:09 +1200

    add server and decompose server/client

---
+++ server/models/models.js
@@ -0,0 +1,48 @@
+const sequelize = require('../db');
+const { DataTypes } = require('sequelize');
+
+const User = sequelize.define('user', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  email: { type: DataTypes.STRING, unique: true },
+  password: { type: DataTypes.STRING },
+  role: { type: DataTypes.STRING, defaultValue: 'User' },
+});
+
+const Basket = sequelize.define('basket', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+});
+
+const BasketDevice = sequelize.define('BasketDevice', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+});
+
+const Device = sequelize.define('device', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING, unique: true, allowNull: false },
+  price: { type: DataTypes.STRING, allowNull: false },
+  rating: { type: DataTypes.STRING, defaultValue: 0 },
+  img: { type: DataTypes.STRING, allowNull: false },
+});
+
+const DeviceInfo = sequelize.define('device_info', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  title: { type: DataTypes.STRING, allowNull: false },
+  description: { type: DataTypes.STRING, allowNull: false },
+});
+
+const Type = sequelize.define('type', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING, unique: true, allowNull: false },
+});
+
+
+const Rating = sequelize.define('rating', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  rate: { type: DataTypes.INTEGER, allowNull: false,  },
+});
+
+// const Brand = sequelize.define('user', {
+//   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+//   name: { type: DataTypes.STRING, unique: true, allowNull: false },
+// });
+
