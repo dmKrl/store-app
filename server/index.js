239e16258b2eedc53f1b45ca9a901f1f28516c36
Dmitry Kralichkin <dm_krl@mail.ru>
2024-08-06 16:38:57 +1200

    create models

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 const express = require('express');
-const sequelize = require('sequelize');
+const sequelize = require('./db');
 // const cors = require('cors');
+const models = require('./models/models');
 
 const PORT = process.env.PORT || 5000;
 
@@ -12,6 +13,8 @@ app.use(express.json());
 
 const start = async () => {
   try {
+    await sequelize.authenticate();
+    await sequelize.sync();
     app.listen(PORT, () => console.log(`Server started on PORT: ${PORT}`));
   } catch (e) {
     console.log(e);
