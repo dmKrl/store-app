d21338053f2569f4ecfcb994eeedecd38fab7310
Dmitry Kralichkin <dm_krl@mail.ru>
2024-08-25 17:36:04 +1200

    change userController - add API for users

--- server/routes/typeRouter.js
+++ server/routes/typeRouter.js
@@ -1,9 +1,9 @@
 const Router = require('express');
 const router = Router();
 const typeController = require('../controllers/typeController');
-// const checkRoleMiddleware = require('../middleware/CheckRoleMiddleware');
+const checkRoleMiddleware = require('../middleware/CheckRoleMiddleware');
 
-router.post('/', typeController.create);
+router.post('/', checkRoleMiddleware, typeController.create);
 router.get('/', typeController.getAll);
 
 module.exports = router;
